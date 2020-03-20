af8605de9977ac2e92e99a90997d0fd5ef8f7458
matan <matanabc@gmail.com>
2020-03-20 08:29:22 +0200

    clean PostConsumer.java

--- app/src/main/java/com/example/opencvvisiontest/vision/PostConsumer.java
+++ app/src/main/java/com/example/opencvvisiontest/vision/PostConsumer.java
@@ -1,11 +1,12 @@
 package com.example.opencvvisiontest.vision;
 
+import com.example.opencvvisiontest.Util;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import java.math.BigDecimal;
@@ -13,6 +14,10 @@ import java.math.RoundingMode;
 import java.util.List;
 
 public class PostConsumer {
+    private double targetCenterX = 0, targetCenterY = 0, xError = 0, yError = 0;
+    private Rect r;
+    private Point targetTopLeft = new Point(0, 0), targetBottomRight = new Point(0, 0);
+
     public Mat execute(Mat threshold, Mat img, List<MatOfPoint> contours) {
         try {
             drawContours(img, contours);
@@ -27,7 +32,7 @@ public class PostConsumer {
         return VisionConstant.showHSV ? threshold : img;
     }
 
-    public void drawFrameCenter(Mat img) {
+    private void drawFrameCenter(Mat img) {
         double center_x = VisionConstant.targetCenterXInFrame;
         double center_y = VisionConstant.targetCenterYInFrame;
 
@@ -37,49 +42,51 @@ public class PostConsumer {
         Imgproc.line(img, new Point(center_x - 10, center_y), new Point(center_x - 20, center_y), VisionConstant.GREEN, 2);
     }
 
-    public void drawContours(Mat img, List<MatOfPoint> contours) {
+    private void drawContours(Mat img, List<MatOfPoint> contours) {
         for (int i = 0; i < contours.size(); i++) {
             Imgproc.drawContours(img, contours, i, VisionConstant.GREEN);
         }
     }
 
-
-    private double center_x, center_y, x1, x2, y1, y2;
-    private Rect r;
-
-    public void drawTarget(Mat img, List<MatOfPoint> contours) {
+    private void drawTarget(Mat img, List<MatOfPoint> contours) {
         r = Imgproc.boundingRect(contours.get(0));
-        x1 = r.tl().x;
-        x2 = r.br().x;
-        y1 = r.tl().y;
-        y2 = r.br().y;
+        targetTopLeft.x = r.tl().x;
+        targetTopLeft.y = r.tl().y;
+        targetBottomRight.x = r.br().x;
+        targetBottomRight.y = r.br().y;
 
         for (int i = 1; i < contours.size(); i++) {
             r = Imgproc.boundingRect(contours.get(i));
-            x1 = Math.min(r.tl().x, x1);
-            x2 = Math.max(r.br().x, x2);
-            y1 = Math.min(r.tl().y, y1);
-            y2 = Math.max(r.br().y, y2);
+            targetTopLeft.x = Math.min(r.tl().x, targetTopLeft.x);
+            targetTopLeft.y = Math.min(r.tl().y, targetTopLeft.y);
+            targetBottomRight.x = Math.max(r.br().x, targetBottomRight.x);
+            targetBottomRight.y = Math.max(r.br().y, targetBottomRight.y);
         }
-        center_x = (x1 + x2) / 2;
-        center_y = (y1 + y2) / 2;
+        targetCenterX = (targetTopLeft.x + targetBottomRight.x) / 2;
+        targetCenterY = (targetTopLeft.y + targetBottomRight.y) / 2;
 
-        Imgproc.rectangle(img, new Point(x1, y1), new Point(x2, y2), VisionConstant.BLUE, 2); //print the area from left point and the right point of the contor that he fond
-        Imgproc.rectangle(img, new Point(center_x, center_y), new Point(center_x, center_y), VisionConstant.RED, 3); //print the centers
+        Imgproc.rectangle(img, targetTopLeft, targetBottomRight, VisionConstant.BLUE, 2);
+        Imgproc.rectangle(img, new Point(targetCenterX, targetCenterY), new Point(targetCenterX, targetCenterY), VisionConstant.RED, 3);
 
-        drawTargetError(img, x1, y1, center_x, center_y);
+        calculateTargetError();
+        drawTargetError(img);
     }
 
-    public void drawTargetError(Mat img, double x, double y, double center_x, double center_y) {
-        Double xError = BigDecimal.valueOf((VisionConstant.targetCenterXInFrame - center_x) * VisionConstant.pixel2AngleX)
+    private void calculateTargetError() {
+        xError = (VisionConstant.targetCenterXInFrame - targetCenterX) * VisionConstant.pixel2AngleX;
+        yError = (VisionConstant.targetCenterYInFrame - targetCenterY) * VisionConstant.pixel2AngleY;
+    }
+
+    private void drawTargetError(Mat img) {
+        double xError2Print = BigDecimal.valueOf(xError)
                 .setScale(3, RoundingMode.HALF_UP)
                 .doubleValue();
 
-        Double yError = BigDecimal.valueOf((VisionConstant.targetCenterYInFrame - center_y) * VisionConstant.pixel2AngleY)
+        double yError2Print = BigDecimal.valueOf(yError)
                 .setScale(3, RoundingMode.HALF_UP)
                 .doubleValue();
 
-        Imgproc.putText(img, "X:" + xError, new Point(x, y - 20), Core.FONT_HERSHEY_SIMPLEX, 0.5, VisionConstant.GREEN, 1);
-        Imgproc.putText(img, "Y:" + yError, new Point(x, y - 8), Core.FONT_HERSHEY_SIMPLEX, 0.5, VisionConstant.GREEN, 1);
+        Imgproc.putText(img, "X:" + xError2Print, new Point(targetTopLeft.x, targetTopLeft.y - 20), Core.FONT_HERSHEY_SIMPLEX, 0.5, VisionConstant.GREEN, 1);
+        Imgproc.putText(img, "Y:" + yError2Print, new Point(targetTopLeft.x, targetTopLeft.y - 8), Core.FONT_HERSHEY_SIMPLEX, 0.5, VisionConstant.GREEN, 1);
     }
 }
